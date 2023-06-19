04a083bb218eb28b53ee5150ca77ccbd2cf33dd9
siddharthagarwal543 <78862240+siddharthagarwal543@users.noreply.github.com>
2023-06-19 10:30:17 +0530

    Create Pythagorean triplet.cpp

---
+++ Pythagorean triplet.cpp
@@ -0,0 +1,6 @@
+/*
+Square each element of number in place and now keep one point to start, one pointer to end and one pointer to last second
+now if square of first and last second comes greater than dec last second pointer
+if smaller than inc first pointer
+else return true
+*/
