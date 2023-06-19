4e2e31b711d41214f810d6e5d786612f791e6bf3
siddharthagarwal543 <78862240+siddharthagarwal543@users.noreply.github.com>
2023-06-19 10:39:06 +0530

    Update Detect Loop in linked list.cpp

--- Detect Loop in linked list.cpp
+++ Detect Loop in linked list.cpp
@@ -14,3 +14,31 @@ public://we will continue iterating list till we don't get second occurence of a
         
     }
 };
+
+class Solution {// TC O(N) SC O(1) floyd's cycle detection algorithm
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL||head->next==NULL||head->next->next==NULL)
+        return false;
+        ListNode *slow=head->next,*fast=head->next->next;
+        while(slow!=NULL&&fast!=NULL){
+            if(slow==fast)
+            return true;
+            if(slow->next==NULL||fast->next==NULL||fast->next->next==NULL)
+            return false;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return false;
+    }
+};
+
+//Similar Problem : https://www.interviewbit.com/problems/list-cycle/
+
+/*
+How do you detect a loop in a linked list?
+A loop can be detected efficiently using the fast and slow pointer algorithm, where the fast pointer moves by two nodes and the slow pointer move by one node at a time.
+
+Will the fast and slow pointer always meet at some point if the list contains a cycle?
+Yes, if the linked list contains a cycle, the fast and slow pointer will always meet at some point.
+*/
